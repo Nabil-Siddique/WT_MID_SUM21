66de232181ab38f775361d34d3a2ae25751c72bc
Nabil <nabilsiddique1999@gmail.com>
2021-06-13 12:05:29 +0600

    prob2

---
+++ Problem2/prob2.php
@@ -0,0 +1,9 @@
+<?php
+$marks =90;
+if($marks>=90) {echo "A+";}
+elseif ($marks>80 && $marks<90) {echo "A";}
+elseif ($marks>70 && $marks<80) {echo "B";}
+elseif ($marks>60 && $marks<70) {echo "C";}
+else {echo "F";}
+
+?>
