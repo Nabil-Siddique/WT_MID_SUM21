fe55bacf3725cb0b826de20f7066083bbfc88677
Nabil <nabilsiddique1999@gmail.com>
2021-06-13 12:49:18 +0600

    prob5

--- Problem4/prob4.php
+++ Problem4/prob4.php
@@ -2,7 +2,7 @@
     $name = "raju";
 
     if($name=="raju" ||  $name=="mina" || $name=="mithu") {echo "<b>hello </b>";
-	echo $name;
+	
 	}
     else
     {
---
+++ Problem5/prob5.php
@@ -0,0 +1,8 @@
+<?php
+    $backColor= "yellow"; $fontColor="cyan"; $imageWidth="300px"; $imageHeight="300px";
+?>
+
+    <img src="aiub_logo.jpg" width="<?php echo $imageWidth;?>" height="<?php echo $imageheight;?>" />
+    <h2>American International University -<span style="background-color:<?php echo $backColor;?>;color:<?php echo $fontColor;?>">
+    Bangladesh</h2>
+</span>
